c2a71054db101f608a279be855ddd2d967808313
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-28 21:30:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,15 +17,15 @@ class School
   
   def add_student(name, grade)
     if @roster.keys.include?(grade)
-      @roster[:grade] << name
+      @roster[grade] << name
     else
-    @roster[:grade] = []
-    @roster[:grade] << name
+    @roster[grade] = []
+    @roster[grade] << name
     end
   end
   
   def grade(grade)
-    @roster[:grade].values
+    @roster[grade].values
   end
   
   
